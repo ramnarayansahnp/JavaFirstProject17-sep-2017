fa56f63bd93311e63560c544c7a8c056d090a9c6
ramnarayansahnp <ramnarayansah3@gmail.com>
2017-09-17 12:59:05 +0530

    first commit from ramnarayansah3@gmail.com

--- JavaFirstProject/src/com/firstdaygithub/HelloFirst.java
+++ JavaFirstProject/src/com/firstdaygithub/HelloFirst.java
@@ -1,7 +1,7 @@
 package com.firstdaygithub;
 
 public class HelloFirst {
-
+//making comment statement from ramnarayansah3@gmail.com github repository.
 	public static void main(String[] args) 
 	{
 		int i=100;
